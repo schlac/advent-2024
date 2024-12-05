cc4954cd551a4826353c6c0972c6a29b49d43938
Christoph Schlameuss <schlac@users.noreply.github.com>
2024-12-05 01:53:10 +0100

    Day 1-2

--- aoc2401/src/main.rs
+++ aoc2401/src/main.rs
@@ -1,17 +1,19 @@
+use std::collections::HashMap;
 use std::fs;
 
 fn main() {
     let file_path = "./input.txt";
     let input = fs::read_to_string(file_path)
         .expect("Should have been able to read the file {file_path}");
-    let c = dist(&input);
-    let m = 0;
+    let c = dist(parse(&input).unwrap());
+    let m = sim(parse(&input).unwrap());
     println!("{c} / {m}");
 }
 
-fn dist(input: &str) -> i64 {
+fn parse(input: &str) -> Option<(Vec<i32>, Vec<i32>)> {
     let mut l: Vec<i32> = vec![];
     let mut r: Vec<i32> = vec![];
+
     for line in input.lines() {
         match line
             .trim_start()
@@ -29,9 +31,28 @@ fn dist(input: &str) -> i64 {
             },
         }
     }
+    Some((l, r))
+}
+
+fn dist(lr: (Vec<i32>, Vec<i32>)) -> i64 {
+    let mut l = lr.0;
+    let mut r = lr.1;
     l.sort();
     r.sort();
     l.iter().zip(r).fold(0, |s,x| s + i64::from((x.0 - x.1).abs()))
+}
+
+fn sim(lr: (Vec<i32>, Vec<i32>)) -> i64 {
+    let l = lr.0;
+    let r = lr.1;
+    let mut rc = HashMap::new();
+    for rv in r.iter() {
+        rc.entry(rv).and_modify(|rc| *rc += 1).or_insert(1);
+    }
+    // for (k,v) in rc.iter() {
+    //     println!("has {k}:{v}");
+    // }
+    l.iter().fold(0, |s,lv| s + (i64::from(*lv) * rc.get(lv).unwrap_or(&0)))
 }
 
 #[cfg(test)]
@@ -44,11 +65,11 @@ fn test1() {
 3 2
 4 4
 ";
-    assert_eq!(dist(input), 1);
+    assert_eq!(dist(parse(input).unwrap()), 1);
 }
 
 #[test]
-fn test() {
+fn test_dist() {
     let input = "
 3   4
 4   3
@@ -57,7 +78,20 @@ fn test() {
 3   9
 3   3
 ";
-    assert_eq!(dist(input), 11);
+    assert_eq!(dist(parse(input).unwrap()), 11);
+}
+
+#[test]
+fn test_sim() {
+    let input = "
+3   4
+4   3
+2   5
+1   3
+3   9
+3   3
+";
+    assert_eq!(sim(parse(input).unwrap()), 31);
 }
 
 }
